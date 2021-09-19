749d04e9832060ca251885b0e07e71092bddee3e
Maksim Ivanov <34414986+ld-hagen@users.noreply.github.com>
2021-09-19 21:33:50 +0300

    Some checks added

--- fix-opera.sh
+++ fix-opera.sh
@@ -18,50 +18,76 @@ readonly CHROME_DL_LINK="https://dl.google.com/linux/direct/google-chrome-stable
 readonly GIT_API_MAIN=https://api.github.com/repos/iteufel/nwjs-ffmpeg-prebuilt/releases
 readonly GIT_API_ALT=https://api.github.com/repos/Ld-Hagen/fix-opera-linux-ffmpeg-widevine/releases
 
-printf '\nGetting Url ...\n'
+if ! which ar > /dev/null && $FIX_WIDEVINE; then
+  printf '\033[1mbinutils\033[0m package must be installed to fix Widevine\n'
+  exit 1
+fi
 
+if ! which unzip > /dev/null; then
+  printf '\033[1munzip\033[0m package must be installed to run this script\n'
+  exit 1
+fi
+
+if ! which wget > /dev/null; then
+  printf '\033[1mwget\033[0m package must be installed to run this script\n'
+  exit 1
+fi
+
+printf 'Getting ffmpeg download Url ...\n'
 readonly OPERA_FFMPEG_URL_MAIN=$(wget -qO - $GIT_API_MAIN | grep browser_download_url | cut -d '"' -f 4 | grep linux-x64 | head -n 1)
 readonly OPERA_FFMPEG_URL_ALT=$(wget -qO - $GIT_API_ALT | grep browser_download_url | cut -d '"' -f 4 | grep linux-x64 | head -n 1)
 
-if [ `basename $OPERA_FFMPEG_URL_ALT` \< `basename $OPERA_FFMPEG_URL_MAIN` ]
-  then
-    readonly OPERA_FFMPEG_URL=$OPERA_FFMPEG_URL_MAIN
-  else
-    readonly OPERA_FFMPEG_URL=$OPERA_FFMPEG_URL_ALT
+if [ `basename $OPERA_FFMPEG_URL_ALT` \< `basename $OPERA_FFMPEG_URL_MAIN` ]; then
+  readonly OPERA_FFMPEG_URL=$OPERA_FFMPEG_URL_MAIN
+else
+  readonly OPERA_FFMPEG_URL=$OPERA_FFMPEG_URL_ALT
 fi
 
-printf '\nDownloading ffmpeg ...\n'
-wget -q --show-progress $OPERA_FFMPEG_URL -O "$TEMP_FILE$ZIP_FILE"
+if [ -z "$OPERA_FFMPEG_URL" ]; then
+  printf 'Failed to get ffmpeg download URL. EXiting...\n'
+  exit 1
+fi
 
-printf "\nUnzipping ...\n\n"
+printf 'Downloading ffmpeg ...\n'
+wget -q --show-progress $OPERA_FFMPEG_URL -O "$TEMP_FILE$ZIP_FILE"
+if [ $? -ne 0 ]; then
+  printf 'Failed to download ffmpeg. Check your internet connection or try later\n'
+  exit 1
+fi
+
+printf "Unzipping ...\n"
 unzip "$TEMP_FILE$ZIP_FILE" -d $TEMP_FILE > /dev/null
 
-printf "\nMoving file on $OPERA_FILE ...\n"
+printf "Moving file on $OPERA_FILE ...\n"
 mkdir -p "$OPERA_FOLDER/lib_extra"
 mv -f "$TEMP_FILE/$FILE_NAME" $OPERA_FILE
 
-printf '\nDeleting Temporary files ...\n'
+printf 'Deleting Temporary files ...\n'
 find $TEMP_FOLDER -name "*$FILE_NAME*" -delete
 
 if $FIX_WIDEVINE
   then
     rm -rf "$OPERA_FOLDER/lib_extra/WidevineCdm"
-    printf  "\nDownloading Google Chrome ...\n"
+    printf  "Downloading Google Chrome ...\n"
     mkdir "$TEMP_FOLDER/chrome"
     cd "$TEMP_FOLDER/chrome"
     wget -q --show-progress "$CHROME_DL_LINK"
+    if [ $? -ne 0 ]; then
+      printf 'Failed to download Google Chrome. Check your internet connection or try later\n'
+      exit 1
+    fi
 
-    printf "\nExtracting Chrome to temporary folder ...\n"
+    printf "Extracting Chrome to temporary folder ...\n"
     CHROME_PKG_NAME=`basename $CHROME_DL_LINK`
     ar x "$CHROME_PKG_NAME"
     tar xf data.tar.xz
 
-    printf "\nInstalling WidevineCdm ...\n"
+    printf "Installing WidevineCdm ...\n"
     cp -R "$TEMP_FOLDER/chrome/opt/google/chrome/WidevineCdm" "$OPERA_FOLDER/lib_extra/"
     printf "[\n      {\n         \"preload\": \"$OPERA_FOLDER/lib_extra/WidevineCdm\"\n      }\n]\n" > "$OPERA_FOLDER/resources/widevine_config.json"
 
-    printf "\nDeleting temprorary files ...\n"
+    printf "Deleting temprorary files ...\n"
     rm -rf "$TEMP_FOLDER/chrome"
   else
-    printf "\nInstalling WidevineCdm skipped\n"
+    printf "Installing WidevineCdm skipped\n"
 fi
